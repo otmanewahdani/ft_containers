a63ed810a65b7b1020676c1e769c1e2d2fdaf311
Otmane Wahdani <otmane.wahdani@gmail.com>
2023-01-24 20:45:52 +0100

    finished testing vector::insert(const_iterator pos, InputIt first, InputIt last)

--- vector/main.cpp
+++ vector/main.cpp
@@ -27,59 +27,60 @@ int main(){
 	//typedef ft::vector<int> Ivec;
 	//typedef ft::vector<std::string> Svec;
 	
-	A arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+	A arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
+		14, 15, 16, 17, 18, 19, 20};
 
 	Avec vec(arr, arr + 14);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
+	std::cout << "size: " << vec.size()  << '\n';
 	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
 	std::cout << '\n';
 
 	vec.insert(vec.begin() + 5, 109, 35);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
+	std::cout << "size: " << vec.size()  << '\n';
 	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
 	std::cout << '\n';
 
-	vec.insert(vec.begin() + 5, 0, 199);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
+	vec.insert(vec.begin() + 5, arr, arr);
+	std::cout << "size: " << vec.size()  << '\n';
 	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
 	std::cout << '\n';
 
-	vec.insert(vec.begin() + 10, 32334, 7834);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
+	vec.insert(vec.begin() + 10, arr, arr + 12);
+	std::cout << "size: " << vec.size()  << '\n';
 	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
 	std::cout << '\n';
 
-	vec.insert(vec.begin() + 1029, 100, 123);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
-	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
-		std::cout << *it << '\n';
-	std::cout << '\n';
-
-	vec.insert(vec.begin() + 4, 78, 123);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
+	vec.insert(vec.end(), arr + 5, arr + 18);
+	std::cout << "size: " << vec.size()  << '\n';
 	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
 	std::cout << '\n';
 
 	vec.insert(vec.end(), 13804, 123);
-	std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << '\n' << '\n';
+	std::cout << "size: " << vec.size()  << '\n';
+	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
+		std::cout << *it << '\n';
+	std::cout << '\n';
+
+	vec.insert(vec.begin() + 4, arr, arr + 20);
+	std::cout << "size: " << vec.size()  << '\n';
 	for (Avec::iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
 	std::cout << '\n';
 
 
-	/*std::istringstream str("12 5 47 734 747 47 7474 445 54 5445 544\
-	544554 545 5 55 5 5 5 5 5 5 5 5 5 5 5 5 5    555   5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 ");
+	std::istringstream str("12 5 47 734 747 47 7474 445 54 5445 544\
+	544554 545 5 55 5 5 5 5 5 5 5 5 5 5 5 5 5    555   5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 89 ");
 	std::istream_iterator<int> it(str);
-	const Avec vec3(it, std::istream_iterator<int>());
-	std::cout << "size: " << vec3.size() << ", capacity: " << vec3.capacity() << '\n' << '\n';
-	for (Avec::const_iterator it = vec3.begin(); it != vec3.end(); it++)
+	vec.insert(vec.begin() + 10, it, std::istream_iterator<int>());
+	std::cout << "size: " << vec.size()  << '\n';
+	for (Avec::const_iterator it = vec.begin(); it != vec.end(); it++)
 		std::cout << *it << '\n';
-	std::cout << '\n';*/
+	std::cout << '\n';
 
 	return 0;
 
