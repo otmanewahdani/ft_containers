9a2be92faa7dbc233d33a0b580d429f5e9d51520
Otmane Wahdani <otmane.wahdani@gmail.com>
2023-02-18 19:24:28 +0100

    created the full definition of List class template

---
+++ map/incs/List.hpp
@@ -0,0 +1,86 @@
+/* this is an internal file included by Queue.hpp.
+	it contains the definition of the List template class.
+	This template class is contains a simple implementation of a singly linked list.
+	It is an allocator-aware container */
+
+#ifndef LIST_HPP
+#define LIST_HPP
+
+#include <memory>
+#include <cstddef>
+
+namespace ft {
+
+	template<
+		class T,
+		class Allocator = std::allocator<T>
+	> class List {
+
+		private:
+			/******* member struct *******/
+			struct Node {
+
+				/******* constructor *******/
+				Node(const T& data, Node* next = NULL)
+					: data(data)
+					, next(next) {}
+				
+				/******* member objects *******/
+				T data;
+				// next node in the list
+				Node* next;
+
+			};
+
+		public:
+			/******* constructor *******/
+			List();
+
+			/******* destructor *******/
+			~List();
+
+			/******* accessors *******/
+			// returns element in the beginning of list
+			T& front();
+
+			// number of elements in list
+			std::size_t size();
+
+			// checks whether list is empty
+			bool isEmpty();
+
+			/******* modifiers *******/
+			// appends data to list
+			void add_back(const T& data);
+
+			// removes element from beginning of list
+			void pop_front();
+
+			// removes all elements fron list
+			void clear();
+
+		private:
+			/******* member types *******/
+			typedef typename Allocator::template
+				rebind<Node>::other node_allocator_type;
+
+			/******* member objects *******/
+			// first element in list
+			Node* mHead;
+			
+			// last element in list
+			Node* mBack;
+
+			// number of nodes in List
+			std::size_t mNodeCount;
+
+			// object used to allocate memory for nodes
+			node_allocator_type mAllocator;
+
+	};
+
+}
+
+#include <List.tpp>
+
+#endif
---
+++ map/incs/List.tpp
@@ -0,0 +1,7 @@
+/* this is an internal file included by List.hpp.
+	it contains the implementation of the List template class.*/
+
+#ifndef LIST_HPP
+#define LIST_HPP
+
+#endif
